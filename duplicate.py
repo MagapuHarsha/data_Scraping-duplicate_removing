1e61679172766da7a03728f2f3a188c003d58667
Magapu Harsha <34785845+MagapuHarsha@users.noreply.github.com>
2021-07-15 12:20:25 +0530

    Update duplicate.py

--- duplicate.py
+++ duplicate.py
@@ -1,5 +1,5 @@
 import pandas as pd
 pd.set_option('display.max_rows',2800)
 pd.set_option('display.max_columns',10)
-df = pd.read_csv('eco_friendly_labeled.csv')
+df = pd.read_csv('file_name.csv')
 df[~df.duplicated(subset=['name'])].to_csv('list(clean).csv')
